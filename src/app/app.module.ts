bb4646c9d9c5a5b0e1b6f3c4bef67d1af9ecbf67
ShivamGupta887466 <shivamg@snapwork.com>
2026-09-11 17:47:57 +0530

    First_commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompoComponent } from './compo/compo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompoComponent
   ],
   imports: [
     BrowserModule,
