c4640a5ed0f3b1c66628c5c00889f3910686acd5
ShivamGupta887466 <shivamg@snapwork.com>
2026-09-19 19:39:26 +0530

    modules-added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,37 @@ import { AppComponent } from './app.component';
 
 const routes: Routes = [
  
+  
+
+  {
+    path:'',
+    loadComponent: () =>
+      import('./layout/layout.component')
+        .then(c => c.LayoutComponent),
+    children:[
+
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+
+
+       
+    { path: 'customers', loadChildren: () => import('./features/customer/customer.module').then(m => m.CustomerModule) },
+ 
+  // { path: 'customers', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) },
+ 
+     { path: 'dashboard', loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule) },
+ 
+     { path: 'user', loadChildren: () => import('./features/user/user.module').then(m => m.UserModule) },
+ 
+
+    ]
+  },
   {
     path:'**',
-    component:AppComponent
+    redirectTo:''
   }
   
 ];
